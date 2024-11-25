f1783c3b93699a972b2fa669b2c32434b900cc33
Aman Juman <aman@amanullahjuman.me>
2024-11-25 20:37:24 +0000

    Update init script to dynamically adjust php version and container IP & remove unwanted command

--- passoire/init.sh
+++ passoire/init.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#Nothing to see here
-if [[ -d /passoire/flags-enc ]]; then
-	/passoire/.h/b.sh
-	/passoire/flags/init.sh
-fi
-
-
-
-# Start DB, web server and ssh server
+# Start DB, web server, and ssh server
 service mysql start
 service ssh start
-service apache2 start
+
+# Detect the installed PHP-FPM version dynamically
+PHP_VERSION=$(php -r "echo PHP_VERSION;" | cut -d '.' -f 1,2)
+PHP_FPM_SOCKET="unix:/var/run/php/php${PHP_VERSION}-fpm.sock"
+
+# Replace the PHP-FPM socket placeholder in the Nginx config
+sed -i "s|CONTAINER_PHP_SOCKET|${PHP_FPM_SOCKET}|g" /etc/nginx/sites-enabled/default
+
+# Start PHP-FPM service
+service php${PHP_VERSION}-fpm start
 
 DB_NAME="passoire"
 DB_USER="passoire"
@@ -21,24 +22,20 @@ DB_PASSWORD=$(head -n 1 /passoire/config/db_pw)
 echo "Creating MySQL database and user..."
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-#mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'localhost' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
-
 mysql -u root ${DB_NAME} < config/passoire.sql
 
-# Redirect querry from website root to our main page
-rm /var/www/html/index.html
-echo "<?php header(\"Location: passoire/index.php\"); ?>" > /var/www/html/index.php
-
-# Link apache dir and our web dir
-ln -s /usr/share/phpmyadmin/ /var/www/html/phpmyadmin
-ln -s /passoire/web/ /var/www/html/passoire
-
-# Adapt to our ip
+# Adapt to our IP
 echo "127.0.0.1 db" >> /etc/hosts
 
+# Replace placeholders in application files
+sed -i "s/CONTAINER_IP/$HOST/g" /passoire/web/crypto.php
+sed -i "s/CONTAINER_IP/$HOST/g" /passoire/crypto-helper/server.js
+sed -i "s/CONTAINER_IP/$HOST/g" /etc/nginx/sites-enabled/default
 
+# Start Nginx
+service nginx start
 
 if [ -z "$HOST" ]; then
   HOST=$(hostname -i)
@@ -46,13 +43,10 @@ fi
 
 echo "Web server running at http://$HOST"
 
-sed -i "s/CONTAINER_IP/$HOST/g" /passoire/web/crypto.php
-sed -i "s/CONTAINER_IP/$HOST/g" /passoire/crypto-helper/server.js
-
 touch /passoire/logs/crypto-helper.log
 
-# Start crypto helper api
+# Start crypto helper API
 /passoire/crypto-helper/crypto-helper.sh start
 
 # Monitor logs
-tail -f /passoire/logs/crypto-helper.log /var/log/apache2/access.log /var/log/apache2/error.log
+tail -f /passoire/logs/crypto-helper.log /var/log/nginx/passoire-access.log /var/log/nginx/passoire-error.log
